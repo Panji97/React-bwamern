d2ccd4c4ea6f7136f596206b87e0067f9c5a94ab
Panji97 <76486961+Panji97@users.noreply.github.com>
2021-07-18 22:47:25 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import 'assets/scss/style.scss';
+
+function App() {
+  return <div className="App">Hallo Dunia</div>;
+}
+
+export default App;
---
+++ src/parts/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Button from 'elements/Button';
+import BrandIcon from 'parts/IconText';
+
+export default function Header(props) {
+  return;
+  <header className="spacing-sm">
+    <div className="container">
+      <nav className="navbar navbar-expand-lg navbar-light">
+        <BrandIcon />
+      </nav>
+      <div className="collapse navbar-collapse">
+        <ul></ul>
+      </div>
+    </div>
+  </header>;
+}
