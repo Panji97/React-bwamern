4144a9b64b204e580756f25e2c964ea02b3cc771
Panji97 <76486961+Panji97@users.noreply.github.com>
2021-07-19 09:12:48 +0700

    buat header dan meletakkandi landingpage

--- src/parts/Header.js
+++ src/parts/Header.js
@@ -3,15 +3,41 @@ import Button from 'elements/Button';
 import BrandIcon from 'parts/IconText';
 
 export default function Header(props) {
-  return;
-  <header className="spacing-sm">
-    <div className="container">
-      <nav className="navbar navbar-expand-lg navbar-light">
-        <BrandIcon />
-      </nav>
-      <div className="collapse navbar-collapse">
-        <ul></ul>
+  const getNavLinkClass = (path) => {
+    return props.location.pathname === path ? ' active' : '';
+  };
+  return (
+    <header className="spacing-sm">
+      <div className="container">
+        <nav className="navbar navbar-expand-lg navbar-light">
+          <BrandIcon />
+
+          <div className="collapse navbar-collapse">
+            <ul className="navbar-nav ml-auto">
+              <li className={`nav-item ${getNavLinkClass('/')}`}>
+                <Button className="nav-link" type="link" href="/">
+                  Home
+                </Button>
+              </li>
+              <li className={`nav-item ${getNavLinkClass('/browse-by')}`}>
+                <Button className="nav-link" type="link" href="/browse-by">
+                  Browse By
+                </Button>
+              </li>
+              <li className={`nav-item ${getNavLinkClass('/stories')}`}>
+                <Button className="nav-link" type="link" href="/stories">
+                  Stories
+                </Button>
+              </li>
+              <li className={`nav-item ${getNavLinkClass('/agents')}`}>
+                <Button className="nav-link" type="link" href="/agents">
+                  Agents
+                </Button>
+              </li>
+            </ul>
+          </div>
+        </nav>
       </div>
-    </div>
-  </header>;
+    </header>
+  );
 }
