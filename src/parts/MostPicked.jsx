ba2696bbf8c6b9be838e048bb34788f9aba65eeb
Panji97 <76486961+Panji97@users.noreply.github.com>
2021-08-05 00:51:07 +0700

    menambahkan foto most picked ke project

--- src/parts/MostPicked.jsx
+++ src/parts/MostPicked.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Button from 'elements/Button';
+import { Link } from 'react-router-dom';
 
 export default function MostPicked(props) {
   return (
@@ -6,21 +8,36 @@ export default function MostPicked(props) {
       <section className="container">
         <h4 className="mb-3">Most Picked</h4>
         <div className="container-grid">
-          <div className="item column-4 row-2">
-            <div className="card">Card 1</div>
-          </div>
-          <div className="item column-4 row 2">
-            <div className="card">Card 2</div>
-          </div>
-          <div className="item column-4 row 2">
-            <div className="card">Card 3</div>
-          </div>
-          <div className="item column-4 row 2">
-            <div className="card">Card 4</div>
-          </div>
-          <div className="item column-4 row 2">
-            <div className="card">Card 5</div>
-          </div>
+          {props.data.map((item, index) => {
+            return (
+              <div className={`item column-4 ${index === 0 ? ' row-2' : ' row-1'}`}>
+                <div className="card card-featured">
+                  <div className="tag">
+                    ${item.price}
+                    <span className="font-weight-light">per {item.unit}</span>
+                  </div>
+                  <figure className="img-wrapper">
+                    <img
+                      src={item.imageUrl}
+                      alt={item.name}
+                      className="img-cover" />
+                  </figure>
+                  <div className="meta-wrapper">
+                    <Button
+                      type={Link}
+                      className="streched-link d-block text-white"
+                      href={`properties/${item._id}`}
+                      >
+                      <h5>{item.name}</h5>
+                    </Button>
+                    <span>
+                      {item.city},{item.country}
+                    </span>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </section>
     </div>
