dd7618bb6fc64387d38efc58ab07686a849bf198
Panji97 <76486961+Panji97@users.noreply.github.com>
2021-08-07 14:35:41 +0700

    membuat most picked dan categories

--- src/parts/MostPicked.jsx
+++ src/parts/MostPicked.jsx
@@ -1,45 +1,35 @@
 import React from 'react';
 import Button from 'elements/Button';
-import { Link } from 'react-router-dom';
 
 export default function MostPicked(props) {
   return (
-    <div>
-      <section className="container">
-        <h4 className="mb-3">Most Picked</h4>
-        <div className="container-grid">
-          {props.data.map((item, index) => {
-            return (
-              <div className={`item column-4 ${index === 0 ? ' row-2' : ' row-1'}`}>
-                <div className="card card-featured">
-                  <div className="tag">
-                    ${item.price}
-                    <span className="font-weight-light">per {item.unit}</span>
-                  </div>
-                  <figure className="img-wrapper">
-                    <img
-                      src={item.imageUrl}
-                      alt={item.name}
-                      className="img-cover" />
-                  </figure>
-                  <div className="meta-wrapper">
-                    <Button
-                      type={Link}
-                      className="streched-link d-block text-white"
-                      href={`properties/${item._id}`}
-                      >
-                      <h5>{item.name}</h5>
-                    </Button>
-                    <span>
-                      {item.city},{item.country}
-                    </span>
-                  </div>
+    <section className="container" ref={props.refMostPicked}>
+      <h4 className="mb-3">Most Picked</h4>
+      <div className="container-grid">
+        {props.data.map((item, index) => {
+          return (
+            <div key={`mostpicked-${index}`} className={`item column-4 ${index === 0 ? ' row-2' : ' row-1'}`}>
+              <div className="card card-featured">
+                <div className="tag">
+                  ${item.price}
+                  <span className="font-weight-light">per {item.unit}</span>
+                </div>
+                <figure className="img-wrapper">
+                  <img src={item.imageUrl} alt={item.name} className="img-cover" />
+                </figure>
+                <div className="meta-wrapper">
+                  <Button type="link" className="stretched-link d-block text-white" href={`properties/${item._id}`}>
+                    <h5>{item.name}</h5>
+                  </Button>
+                  <span>
+                    {item.city},{item.country}
+                  </span>
                 </div>
               </div>
-            );
-          })}
-        </div>
-      </section>
-    </div>
+            </div>
+          );
+        })}
+      </div>
+    </section>
   );
 }
